1990e7ce5f675ee23a18c86d47af328f5824056c
lonelymaple <cytrikov.m@mail.ru>
2023-12-28 14:00:56 +0700

    code

--- app/src/main/java/com/example/tsytrikov_2/MainActivity.java
+++ app/src/main/java/com/example/tsytrikov_2/MainActivity.java
@@ -8,17 +8,18 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText name = findViewById(R.id.editTextTextPersonName2);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
     public void goToPersonalArea(View view)
     {
+        EditText name = findViewById(R.id.editTextTextPersonName2);
+
         Intent intent = new Intent(this, personal_area.class);
-        intent.putExtra("UserName", name.getText());
+        intent.putExtra("UserName", name.getText().toString());
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/tsytrikov_2/personal_area.java
+++ app/src/main/java/com/example/tsytrikov_2/personal_area.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.widget.TextView;
 
 public class personal_area extends AppCompatActivity {
-    TextView tv = findViewById(R.id.HelloUser);
-    Bundle arguments = getIntent().getExtras();
-    String name = arguments.get("name").toString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView tv = findViewById(R.id.HelloUser);
         setContentView(R.layout.activity_personal_area);
     }
     public void goToMain(View view)
